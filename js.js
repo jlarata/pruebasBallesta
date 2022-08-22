dda1ff08248789cb14c222916cc110b47148dab3
Ariel <ariel@cfee.gob.ar>
2022-08-22 15:06:47 -0300

    esqueleto
    :wq
    Please enter the commit message for your changes. Lines starting

---
+++ js.js
@@ -0,0 +1,22 @@
+
+
+
+var variable1=0;
+
+const constante1=document.getElementById("constante");
+
+function inicio(){
+
+function meteImagenYRezisea() {
+    marco = document.createElement("iframe");
+    marco.classList.add("PONERLEUNNOMBRE");
+    node = document.createTextNode("lalala dale boke.");
+    marco.appendChild(node); <--- EN LUGAR DE UN NODO TEXTO UN STYLE?
+    
+    function cartelin() {
+    marco.style.height="16vh";
+    }
+    }
+    
+
+};
